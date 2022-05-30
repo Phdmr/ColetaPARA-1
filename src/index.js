58bb4cb94f25a84317eb8bff948f87d7184d3238
JonathanSaless <51421355+JonathanSaless@users.noreply.github.com>
2022-05-30 10:06:40 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,133 @@
 import interact from "https://cdn.interactjs.io/v1.9.20/interactjs/index.js";
+var infoPontos = document.getElementById('score')
+var infoVida = document.getElementById('life')
+var pontos = 0
+var vida = '&#128153&#128153&'
 
-interact(".trash").dropzone({
-  accept: ".object",
+interact(".trashPlastico").dropzone({
+  accept: ".objPlastico, .objMetal, .objVidro, .objPapel, .objOrganico",
   ondrop: (event) => {
     var draggableElement = event.relatedTarget;
     draggableElement.style.visibility = "hidden";
-    console.log("Dropado");
+  if (draggableElement.className == 'objPlastico notSelected'){
+    console.log("Dropado")
+    pontuou()
+  }else{
+    console.log("Dropado errado")
+    perde_vida()
+  }
   },
 });
 
-interact(".object").draggable({
-  // movimentação suave
+interact(".objPlastico").draggable({
   inertia: true,
-
-  /*    MODIFICADORES
-  Define que apenas o determinado objeto será movimentado.
-  */
   modifiers: [
     interact.modifiers.restrictRect({
       endOnly: true,
     }),
   ],
+  listeners: { move: dragMoveListener },
+});
 
-  /*    OBSERVADORES
-  Determina o que deve ser executado enquanto uma ação de movimentação
-  estiver acontecendo.
-  */
+interact(".trashMetal").dropzone({
+  accept: ".objPlastico, .objMetal, .objVidro, .objPapel, .objOrganico",
+  ondrop: (event) => {
+    var draggableElement = event.relatedTarget;
+    draggableElement.style.visibility = "hidden";
+  if (draggableElement.className == 'objMetal notSelected'){
+    console.log("Dropado")
+    pontuou()
+  }else{
+    console.log("Dropado errado")
+  }
+  },
+});
+
+interact(".objMetal").draggable({
+  inertia: true,
+  modifiers: [
+    interact.modifiers.restrictRect({
+      endOnly: true,
+    }),
+  ],
+  listeners: { move: dragMoveListener },
+});
+
+interact(".trashVidro").dropzone({
+  accept: ".objPlastico, .objMetal, .objVidro, .objPapel, .objOrganico",
+  ondrop: (event) => {
+    var draggableElement = event.relatedTarget;
+    draggableElement.style.visibility = "hidden";
+  if (draggableElement.className == 'objVidro notSelected'){
+    console.log("Dropado")
+    pontuou()
+  }else{
+    console.log("Dropado errado")
+  }
+  },
+});
+
+interact(".objVidro").draggable({
+  inertia: true,
+  modifiers: [
+    interact.modifiers.restrictRect({
+      endOnly: true,
+    }),
+  ],
+  listeners: { move: dragMoveListener },
+});
+
+interact(".trashPapel").dropzone({
+  accept: ".objPlastico, .objMetal, .objVidro, .objPapel, .objOrganico",
+  ondrop: (event) => {
+    var draggableElement = event.relatedTarget;
+    draggableElement.style.visibility = "hidden";
+  if (draggableElement.className == 'objPapel notSelected'){
+    console.log("Dropado")
+    pontuou()
+  }else{
+    console.log("Dropado errado")
+  }
+  },
+});
+
+interact(".objPapel").draggable({
+  inertia: true,
+  modifiers: [
+    interact.modifiers.restrictRect({
+      endOnly: true,
+    }),
+  ],
+  listeners: { move: dragMoveListener },
+});
+
+interact(".trashOrganico").dropzone({
+  accept: ".objPlastico, .objMetal, .objVidro, .objPapel, .objOrganico",
+  ondrop: (event) => {
+    var draggableElement = event.relatedTarget;
+    draggableElement.style.visibility = "hidden";
+  if (draggableElement.className == 'objOrganico notSelected'){
+    console.log("Dropado")
+    pontuou()
+  }else{
+    console.log("Dropado errado")
+  }
+  },
+});
+
+interact(".objOrganico").draggable({
+  inertia: true,
+  modifiers: [
+    interact.modifiers.restrictRect({
+      endOnly: true,
+    }),
+  ],
   listeners: { move: dragMoveListener },
 });
 
 function dragMoveListener(event) {
   let target = event.target;
-  // calcula a positção final do objeto
+  // calcula a posição final do objeto
   let x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
   let y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
 
@@ -43,7 +139,7 @@ function dragMoveListener(event) {
   target.setAttribute("data-y", y);
 }
 
-function iniciarJogo() {
+/*function iniciarJogo() {
   var goFS = document.getElementById("goFS");
   goFS.addEventListener(
     "click",
@@ -53,4 +149,12 @@ function iniciarJogo() {
     },
     false
   );
+}*/
+function pontuou(){
+  pontos += 1000
+  infoPontos.innerHTML = pontos
+}
+function perde_vida(){
+  //vida = RETIRAR UMA VIDA -> '&#128153'
+  infoVida.innerHTML = vida
 }
